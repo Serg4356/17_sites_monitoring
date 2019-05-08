71b1f2f59379d72f0aebbe4cb694193ff40c11ce
Serg4356 <asdfg111000@yandex.ru>
2019-05-08 16:46:49 +0500

    Add requirements.txt, rename wrong-named functions, add error handling, add How to install section in README.md, rewrite description of project, remove magic numbers, refactor code

--- README.md
+++ README.md
@@ -2,11 +2,63 @@
 
 This script checks for sites status. For each url in url.txt script displays if server responds with HTTP 200 and information about expiration date.
 
+### How to install
+
+python has to be installed on your system. Use pip (or pip3 if there is conflict with Python 2) to install dependences.
+```
+pip install -r requirements.txt
+```
+It is recommended to use virtual environment [virtualenv/venv](https://docs.python.org/3/library/venv.html) to isolate your project 
+
+
 ### Quickstart
-Input into urls.txt urls you need to check and type into your console:
+
+Programm provides a console interface. You can type -h to call interface description.
+
+The interface has three main arguments:
+'-u', '--url' - string argument - you can type 1 url directly from console.
+'-p', '--path' - string argument - you can type path to txt file with url, which you want to check.
+'-d', '--days' - integer argument - you can pecify number of days domain should be paid for. Default value is 30 days.
+
+'-u' and '-p' arguments are mutually exclusive, so you must choose only one way to procceed.
+
+For example you have file: urls.txt in your project folder, which looks like this:
 ```
-$python check_sites_health.py
+http://wikipedia.org
+http://coursera.com
+http://roga.ikopyta
 ```
+type into your console:
+```
+$python check_sites_health.py -p urls.txt -d 60
+```
+The output should look like this:
+```
+url: http://wikipedia.org
+server response ok: True
+domain expiration date: Domain name has been paid for more than 60 days
+url: http://coursera.com
+server response ok: True
+domain expiration date: Domain name has been paid for more than 60 days
+A Connection Error occured while sending request to:
+http://roga.ikopyta
+May be there is a mistake in url
+url: http://roga.ikopyta
+server response ok: False
+domain expiration date: Whois has no information about expiration date
+```
+
+Also you can type:
+```
+$python check_sites_health.py -u http://wikipedia.org -d 60
+```
+You'll get:
+```
+url: http://wikipedia.org
+server response ok: True
+domain expiration date: Domain name has been paid for more than 60 days
+```
+
 
 ### Project Goals
 
--- check_sites_health.py
+++ check_sites_health.py
@@ -5,7 +5,6 @@ import types
 import argparse
 
 
-
 def create_parser():
     parser = argparse.ArgumentParser(
         description='''
@@ -14,7 +13,15 @@ def create_parser():
         ''')
     group = parser.add_mutually_exclusive_group(required=True)
     group.add_argument('-u', '--url', help='Input site url')
-    group.add_argument('-p', '--path', help='Input path to txt file with urls')
+    group.add_argument('-p',
+                       '--path',
+                       help='Input path to txt file with urls')
+    parser.add_argument('-d',
+                        '--days',
+                        type=int,
+                        default=30,
+                        help='Days of domain expiration'
+                        ' check (30 for default)')
 
     return parser
 
@@ -24,7 +31,7 @@ def load_urls4check(path):
         return [url for url in urls_file.read().split('\n') if url]
 
 
-def is_server_respond_with_200(url):
+def is_server_respond_ok(url):
     response = requests.get(url)
     return response.ok
 
@@ -33,26 +40,40 @@ def get_domain_expiration_date(domain_name):
     expiration_date = whois.whois(domain_name).expiration_date
     if isinstance(expiration_date, list):
         expiration_date = expiration_date[-1]
-    elif isinstance(expiration_date, type(None)):
+    return expiration_date
+
+
+def is_expired(expiration_date, days_before_expiration):
+    if not expiration_date:
         return 'Whois has no information about expiration date'
     time_delta = expiration_date.replace(tzinfo=None) - datetime.datetime.now()
-    if time_delta.days > 30:
-        return 'Domain name has been paid for more than 30 days'
+    if time_delta.days > days_before_expiration:
+        return ('Domain name has been paid for'
+                ' more than {} days'.format(days_before_expiration))
     else:
-        return 'Domain name payment expires in 30 days'
+        return f'Domain name payment expires in {days_before_expiration} days'
 
 
 if __name__ == '__main__':
-	parser = create_parser()
-	arguments = parser.parse_args()
-	if arguments.url is None:
-		urls4check = load_urls4check(arguments.path)
-	else:
-		urls4check = [arguments.url]
-	for url in urls4check:
-		domain = url.split('/')[-1]
-		print('url: {}\nserver response ok: {}\ndomain '
-			'expiration date: {}'.format(url,
-                                         is_server_respond_with_200(url),
-                                         get_domain_expiration_date(domain)))
-
+    parser = create_parser()
+    arguments = parser.parse_args()
+    if arguments.url is None:
+        urls4check = load_urls4check(arguments.path)
+    else:
+        urls4check = [arguments.url]
+    for url in urls4check:
+        domain = url.split('/')[-1]
+        server_respond_ok = False
+        try:
+            server_respond_ok = is_server_respond_ok(url)
+        except requests.exceptions.ConnectionError:
+            print('A Connection Error occured while sending request to:')
+            print(url)
+            print('May be there is a mistake in url')
+        domain_expiration_date = get_domain_expiration_date(domain)
+        print(('url: {}\nserver response ok: {}\ndomain '
+               'expiration date: {}'.format(url,
+                                            server_respond_ok,
+                                            is_expired(
+                                                domain_expiration_date,
+                                                arguments.days))))

ca7ee1d97f38828075ba706e7225d37ef4cabad8
Serg4356 <asdfg111000@yandex.ru>
2019-05-10 00:01:29 +0500

    Simplify main by making additional function

--- check_sites_health.py
+++ check_sites_health.py
@@ -49,6 +49,17 @@ def is_expired(expiration_date, days_before_expiration):
     return time_delta.days > days_before_expiration
 
 
+def make_expiration_message(expired_status, days_before_expiration):
+    if expired_status is None:
+        return 'Whois has no information about expiration date'
+    elif expired_status:
+        return 'Domain name has been paid for more than {} days'.format(
+            days_before_expiration)
+    else:
+        return 'Domain name payment expires in {} days'.format(
+            days_before_expiration)
+
+
 if __name__ == '__main__':
     parser = create_parser()
     arguments = parser.parse_args()
@@ -67,15 +78,8 @@ if __name__ == '__main__':
             print('May be there is a mistake in url')
         domain_expiration_date = get_domain_expiration_date(domain)
         expired_status = is_expired(domain_expiration_date, arguments.days)
-        if expired_status is None:
-            expired_status_message = ('Whois has no information'
-                                      ' about expiration date')
-        elif expired_status:
-            expired_status_message = ('Domain name has been paid for more '
-                                      'than {} days'.format(arguments.days))
-        else:
-            expired_status_message = ('Domain name payment expires in '
-                                      '{} days'.format(arguments.days))
+        expired_status_message = make_expiration_message(expired_status,
+                                                         arguments.days)
         print(('url: {}\nserver response ok: {}\ndomain '
                'expiration date: {}'.format(url,
                                             server_respond_ok,
